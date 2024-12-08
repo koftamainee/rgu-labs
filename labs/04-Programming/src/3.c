ca1f6215441c1d9deaa2f26a5641931430609a68
koftamainee <koftamainee@gmail.com>
2024-12-08 12:50:27 +0300

    feature/mail creation and deletion

--- labs/04-Programming/src/3.c
+++ labs/04-Programming/src/3.c
@@ -5,8 +5,11 @@
 
 #include "../../../libc/cstring.h"
 #include "../../../libc/errors.h"
+#include "../../../libc/input.h"
+#include "../../../libc/memory.h"
 
 #define INITIAL_CAPACITY (16)
+#define GROWTH_FACTOR (2)
 
 typedef struct Address {
     String city;
@@ -14,6 +17,7 @@ typedef struct Address {
     size_t house_number;
     String body;
     size_t flat_number;
+    String index;
 } Address;
 
 typedef struct Mail {
@@ -33,13 +37,14 @@ typedef struct Post {
 
 // CLI utils
 err_t read_string_from_user(String *str);
-err_t read_double_from_user(int *num);
-err_t read_size_t_from_user(int *num);
+err_t read_double_from_user(double *num);
+err_t read_size_t_from_user(size_t *num);
 
 // Creators
 err_t post_init(Post *p);
-err_t mail_init(Mail *m, String id);
-err_t address_init(Address *a);
+
+// Utils
+err_t post_comparer(const void *a, const void *b);
 
 // Destructors
 void post_destructor(Post *p);
@@ -51,8 +56,8 @@ err_t start_interactive_menu(Post *p);
 err_t send_mail(Post *p);
 err_t obtain_mail_by_id(Post *p);
 err_t delete_mail(Post *p);
-err_t get_delivered_mails(Post *p, Mail **m);
-err_t get_not_delivered_mails(Post *p, Mail **m);
+err_t get_delivered_mails(Post *p);
+err_t get_not_delivered_mails(Post *p);
 
 int program_04_3(int argc, char *argv[]) {
     err_t err = 0;
@@ -83,7 +88,7 @@ err_t read_string_from_user(String *str) {
     }
 
     if (*str != NULL) {  // potentioal memory leak
-        printf("WARNING: POTENCIAL MEMORY LEAK\n");
+        fprintf(stderr, "WARNING: POTENCIAL MEMORY LEAK\n");
     }
 
     if (!fgets(buf, BUFSIZ, stdin)) {
@@ -91,18 +96,17 @@ err_t read_string_from_user(String *str) {
     }
     len = strlen(buf);
     if (len > 0 && buf[len - 1] == '\n') {
-        buf[len = 1] = '\0';  // delete '\n' symbol
+        buf[len - 1] = '\0';  // delete '\n' symbol
     }
 
     *str = string_from(buf);  // allocating memory for string
     if (*str == NULL) {
         return MEMORY_ALLOCATION_ERROR;
     }
-
     return EXIT_SUCCESS;
 }
 
-err_t read_double_from_user(int *num) {
+err_t read_double_from_user(double *num) {
     char buf[64];  // 32 bytes buf for int32_t
     char *endptr = NULL;
 
@@ -119,8 +123,8 @@ err_t read_double_from_user(int *num) {
     return EXIT_SUCCESS;
 }
 
-err_t read_size_t_from_user(int *num) {
-    char buf[32];  // 32 bytes buf for int32_t
+err_t read_size_t_from_user(size_t *num) {
+    char buf[64];  // 32 bytes buf for int32_t
     char *endptr = NULL;
 
     if (num == NULL) {
@@ -143,12 +147,10 @@ err_t post_init(Post *p) {
 
     p->post_office_address = (Address *)malloc(sizeof(Address));
     if (p->post_office_address == NULL) {
-        printf("123");
         return MEMORY_ALLOCATION_ERROR;
     }
     p->mails = (Mail *)malloc(INITIAL_CAPACITY * sizeof(Mail));
     if (p->mails == NULL) {
-        printf("12345");
         return MEMORY_ALLOCATION_ERROR;
     }
 
@@ -156,10 +158,6 @@ err_t post_init(Post *p) {
     p->__mails_capacity = INITIAL_CAPACITY;
     return EXIT_SUCCESS;
 }
-
-err_t mail_init(Mail *m, String id) {}
-
-err_t address_init(Address *a) {}
 
 void post_destructor(Post *p) {
     size_t i = 0;
@@ -181,16 +179,284 @@ void address_destructor(Address *a) {
     string_free(a->city);
     string_free(a->street);
     string_free(a->body);
+    string_free(a->index);
 }
 
-err_t start_interactive_menu(Post *p) {}
+err_t start_interactive_menu(Post *p) {
+    char c;
+    err_t err;
+    size_t user_ans;
+    if (p == NULL) {
+        return DEREFERENCING_NULL_PTR;
+    }
+    while (1) {
+        clear_screen();
+        printf(
+            "Choose option: \n"
+            "1. Send mail.\n"
+            "2. Obtain mail by id.\n"
+            "3. Delete mail by id.\n"
+            "4. Get delivered mail.\n"
+            "5. Get not delivered mail.\n"
+            "0. Exit\n\nInput number: ");
+        while (c != EOF && (c = getchar()) != '\n');
+        err = read_size_t_from_user(&user_ans);
+        if (err) {
+            return err;
+        }
+        switch (user_ans) {
+            case 1:
+                err = send_mail(p);
+                if (err) {
+                    return err;
+                }
+                break;
+            case 2:
+                err = obtain_mail_by_id(p);
+                if (err) {
+                    return err;
+                }
+                break;
+            case 3:
+                err = delete_mail(p);
+                if (err) {
+                    return err;
+                }
+                break;
+            case 4:
+                err = get_delivered_mails(p);
+                if (err) {
+                    return err;
+                }
+                break;
+            case 5:
+                err = get_not_delivered_mails(p);
+                if (err) {
+                    return err;
+                }
+                break;
+            case 0:
+                return EXIT_SUCCESS;
+            default:
+                return INVALID_FLAG;
+        }
+        fflush(stdin);
+    }
+    return EXIT_SUCCESS;
+}
 
-err_t send_mail(Post *p) {}
+err_t send_mail(Post *p) {
+    String s_ans = NULL;
+    size_t st_ans = 0;
+    double d_ans;
 
-err_t obtain_mail_by_id(Post *p) {}
+    err_t err;
 
-err_t delete_mail(Post *p) {}
+    if (p->mails_count + 1 > p->__mails_capacity) {
+        err = rerealloc((void **)&(p->mails),
+                        p->__mails_capacity * GROWTH_FACTOR * sizeof(Mail));
+        if (err) {
+            return err;
+        }
+        p->__mails_capacity *= GROWTH_FACTOR;
+    }
 
-err_t get_delivered_mails(Post *p, Mail **m) {}
+    // p->mails[p->mails_count].id;
+    printf("Enter id: ");
+    fflush(stdin);
+    err = read_string_from_user(&s_ans);
+    if (err) {
+        string_free(s_ans);
+        return err;
+    }
+    p->mails[p->mails_count].id = s_ans;
+    s_ans = NULL;
 
-err_t get_not_delivered_mails(Post *p, Mail **m) {}
+    printf("RECIPIENT INFO\n");
+    printf("Enter city: ");
+    fflush(stdin);
+    err = read_string_from_user(&s_ans);
+    if (err) {
+        string_free(s_ans);
+        return err;
+    }
+    p->mails[p->mails_count].recipient_adress.city = s_ans;
+    s_ans = NULL;
+    printf("Enter street: ");
+    fflush(stdin);
+    err = read_string_from_user(&s_ans);
+    if (err) {
+        string_free(s_ans);
+        return err;
+    }
+    p->mails[p->mails_count].recipient_adress.street = s_ans;
+    s_ans = NULL;
+
+    printf("Enter house number: ");
+    fflush(stdin);
+    err = read_size_t_from_user(&st_ans);
+    if (err) {
+        return err;
+    }
+    p->mails[p->mails_count].recipient_adress.house_number = st_ans;
+    st_ans = 0;
+
+    printf("Enter body: ");
+    fflush(stdin);
+    err = read_string_from_user(&s_ans);
+    if (err) {
+        string_free(s_ans);
+        return err;
+    }
+    p->mails[p->mails_count].recipient_adress.body = s_ans;
+    s_ans = NULL;
+
+    printf("Enter flat number: ");
+    fflush(stdin);
+    err = read_size_t_from_user(&st_ans);
+    if (err) {
+        return err;
+    }
+    p->mails[p->mails_count].recipient_adress.flat_number = st_ans;
+    st_ans = 0;
+
+    printf("Enter index: ");
+    fflush(stdin);
+    err = read_string_from_user(&s_ans);
+    if (err) {
+        string_free(s_ans);
+        return err;
+    }
+    p->mails[p->mails_count].recipient_adress.index = s_ans;
+    s_ans = NULL;
+
+    printf("Enter package weight: ");
+    fflush(stdin);
+    err = read_double_from_user(&d_ans);
+    if (err) {
+        return err;
+    }
+    p->mails[p->mails_count].weight = d_ans;
+    d_ans = 0;
+
+    printf("Enter creation time: ");
+    fflush(stdin);
+    err = read_string_from_user(&s_ans);
+    if (err) {
+        string_free(s_ans);
+        return err;
+    }
+    p->mails[p->mails_count].creation_time = s_ans;
+    s_ans = NULL;
+
+    printf("Enter delivery time: ");
+    fflush(stdin);
+    err = read_string_from_user(&s_ans);
+    if (err) {
+        string_free(s_ans);
+        return err;
+    }
+    p->mails[p->mails_count].delivery_time = s_ans;
+    s_ans = NULL;
+
+    p->mails_count++;
+
+    qsort((void *)p->mails, p->mails_count, sizeof(Mail), post_comparer);
+
+    return EXIT_SUCCESS;
+}
+
+err_t obtain_mail_by_id(Post *p) {
+    char c;
+    err_t err;
+    String s_ans = NULL;
+    size_t i;
+
+    printf("Enter id: ");
+    err = read_string_from_user(&s_ans);
+    if (err) {
+        string_free(s_ans);
+        return err;
+    }
+    for (i = 0; i < p->mails_count; ++i) {
+        if (!string_cmp(s_ans, p->mails[i].id)) {
+            printf("Found Mail with id ");
+            string_print(s_ans);
+            printf("\nWeigth: %lf\nCreation time: ", p->mails[i].weight);
+            string_print(p->mails[i].creation_time);
+            printf("\nDelivery time: ");
+            string_print(p->mails[i].delivery_time);
+            printf("\nRecepient: \nCity: ");
+            string_print(p->mails[i].recipient_adress.city);
+            printf("\nStreet: ");
+            string_print(p->mails[i].recipient_adress.street);
+            printf("\nHouse number: %zu\nBody: ",
+                   p->mails[i].recipient_adress.house_number);
+            string_print(p->mails[i].recipient_adress.body);
+            printf("\nFlat number: %zu\nIndex: ",
+                   p->mails[i].recipient_adress.flat_number);
+            string_print(p->mails[i].recipient_adress.index);
+            printf("\nPress enter to continue");
+            while (c != EOF && (c = getchar()) != '\n');
+            string_free(s_ans);
+            return EXIT_SUCCESS;
+        }
+    }
+    printf("Mail with ");
+    string_print(s_ans);
+    printf(" id not exits\n");
+    printf("\nPress enter to continue");
+    while (c != EOF && (c = getchar()) != '\n');
+    string_free(s_ans);
+    return EXIT_SUCCESS;
+}
+
+err_t delete_mail(Post *p) {
+    char c;
+    err_t err;
+    String s_ans = NULL;
+    size_t i;
+
+    printf("Enter id: ");
+    err = read_string_from_user(&s_ans);
+    if (err) {
+        string_free(s_ans);
+        return err;
+    }
+    for (i = 0; i < p->mails_count; ++i) {
+        if (!string_cmp(s_ans, p->mails[i].id)) {
+            memmove(p->mails + i, p->mails + i + 1,
+                    sizeof(Mail) * (p->mails_count - i - 1));
+            p->mails_count--;
+            printf("Mail deleted\n");
+            printf("\nPress enter to continue");
+            while (c != EOF && (c = getchar()) != '\n');
+            string_free(s_ans);
+            return EXIT_SUCCESS;
+        }
+    }
+
+    printf("Mail with ");
+    string_print(s_ans);
+    printf(" id not exits\n");
+    printf("\nPress enter to continue");
+    while (c != EOF && (c = getchar()) != '\n');
+    string_free(s_ans);
+    return EXIT_SUCCESS;
+}
+
+err_t get_delivered_mails(Post *p) {}
+
+err_t get_not_delivered_mails(Post *p) {}
+
+err_t post_comparer(const void *a, const void *b) {
+    int ret;
+    Mail *ap = (Mail *)a;
+    Mail *bp = (Mail *)b;
+    ret = string_cmp(ap->recipient_adress.index, bp->recipient_adress.index);
+    if (ret) {  // strings not equal
+        return ret;
+    }
+    ret = string_cmp(ap->id, bp->id);
+    return ret;
+}
